9b3264e9f60b64e358978ed468cfacccdf156161
Tupik Maxim <m-tupik2006@mail.ru>
2024-10-22 18:47:11 +0700

    удалил файлы опираясь на gitignore

---
+++ AppVki.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VkiPetterns.example;
+
+internal class AppVki{
+    public static void Main(string[] args){
+        Class1 obj = new Class1("моя масага");
+
+        Console.WriteLine(obj.GetHashCode());
+        Console.WriteLine(obj.getMessage());
+    }
+}
---
+++ example/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VkiPetterns.example
+{
+    internal class Class1{
+        private string massage;
+
+        public Class1(string massage){
+            this.massage = massage;
+        }
+
+        public string getMessage(){
+            return massage;
+        }
+    }
+}
